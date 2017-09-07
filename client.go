94d57194054b5a94cbfdc9e231e88f8338a0dc44
vvmk <vincentmasiello@gmail.com>
2017-09-06 21:39:34 -0400

    clean up experiments, write client and router files

---
+++ client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+type FindHandler func(string) (Handler, bool)
+
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+type Client struct {
+	send        chan Message
+	socket      *websocket.Conn
+	findHandler FindHandler
+}
+
+func (client *Client) Read() {
+	var message Message
+	for {
+		if err := client.socket.ReadJSON(message); err != nil {
+			break
+		}
+		// router should expose a function that can look up a handler
+		//then call that function here.
+		if handler, found := client.findHandler(message.Name); found {
+			handler(client, message.Data)
+		}
+	}
+	client.socket.Close()
+}
+func (client *Client) Write() {
+	for msg := range client.send {
+		fmt.Printf("%#v\n", msg)
+		if err := client.socket.WriteJSON(msg); err != nil {
+			break
+		}
+	}
+	client.socket.Close()
+}
+
+func NewClient(socket *websocket.Conn, findHandler FindHandler) *Client {
+	return &Client{
+		send:        make(chan Message),
+		socket:      socket,
+		findHandler: findHandler, //need an ending comma, oh boy
+	}
+}
